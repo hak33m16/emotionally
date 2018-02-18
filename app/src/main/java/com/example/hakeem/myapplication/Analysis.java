79d6d3c5a68e080a37c5881b87b05c3b7c0c84f8
hak33m16 <abadran@kent.edu>
2018-02-18 13:03:25 -0500

    Initialization of Repository

---
+++ app/src/main/java/com/example/hakeem/myapplication/Analysis.java
@@ -0,0 +1,27 @@
+package com.example.hakeem.myapplication;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Hakeem on 2/18/2018.
+ */
+
+public class Analysis {
+
+    private ArrayList<ArrayList<String>> response;
+    private String message;
+    private int id;
+
+    public JSONObject populateResponse() {
+
+        JSONObject analysis = null;
+
+
+
+        return analysis;
+
+    }
+
+}
---
+++ app/src/main/java/com/example/hakeem/myapplication/AnalysisAPIController.java
@@ -0,0 +1,12 @@
+package com.example.hakeem.myapplication;
+
+/**
+ * Created by Hakeem on 2/18/2018.
+ */
+
+public class AnalysisAPIController {
+
+    private Analysis analysis;
+
+
+}
