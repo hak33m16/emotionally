907643deadecd1b7e4cf31d0bc7a32a4069b8532
Michael Garlak <mgarlak@kent.edu>
2018-02-26 22:28:07 -0500

    Calling service request

--- app/src/main/java/com/example/hakeem/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hakeem/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.hakeem.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    
+
+    IBMToneAPI ita = new IBMToneAPI();
+    JSONObject analyzedJson = ita.analyze("Hello, World!");
+    String jString = analyzedJson.toString();
 }
