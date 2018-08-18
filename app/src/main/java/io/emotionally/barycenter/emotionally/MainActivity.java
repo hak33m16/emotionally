9850be6954aa9f5f10838efcbc6f7c4e0266e39d
dbevila1 <dbevila1@kent.edu>
2018-08-17 23:41:21 -0400

    02 create menu item in settings bar

--- app/src/main/java/io/emotionally/barycenter/emotionally/MainActivity.java
+++ app/src/main/java/io/emotionally/barycenter/emotionally/MainActivity.java
@@ -10,6 +10,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -49,6 +52,18 @@ public class MainActivity extends AppCompatActivity {
 
         //apc = new ApplicationController();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 
     ////////////////////////////////////////////////////////////
